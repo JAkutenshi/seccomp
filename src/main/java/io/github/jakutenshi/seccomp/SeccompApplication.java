e8aa63063b61c3e7200fcbd7970d5ba4f6a3916e
JAkutenshi <jakutenshi@gmail.com>
2019-10-08 16:06:13 +0300

    Initial commit and index.html app created

---
+++ src/main/java/io/github/jakutenshi/seccomp/SeccompApplication.java
@@ -0,0 +1,13 @@
+package io.github.jakutenshi.seccomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeccompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeccompApplication.class, args);
+	}
+
+}
