2375899cf350c2bab2762b71759ad086f122b581
JAkutenshi <jakutenshi@gmail.com>
2019-10-20 21:54:31 +0300

    Fix db file creating problem and JpaRepository experience

--- src/main/java/io/github/jakutenshi/seccomp/MainApplication.java
+++ src/main/java/io/github/jakutenshi/seccomp/MainApplication.java
@@ -17,8 +17,10 @@ public class MainApplication {
 	@Bean
 	public CommandLineRunner demo(OrganizationRepository repository) {
 		return args -> {
-			repository.save(new Organization("Рога и копыта", "Торжковская 15", "+79311234567"));
-			for (Organization o : repository.findAll()) {
+
+
+			//repository.save(new Organization("Рога и копыта", "Торжковская 15", "+79311234567"));
+			for (Organization o : repository.findByLegalAddress("Торжковская 15")) {
 				System.out.println(o.toString());
 			}
 		};
--- src/main/java/io/github/jakutenshi/seccomp/organizations/OrganizationRepository.java
+++ src/main/java/io/github/jakutenshi/seccomp/organizations/OrganizationRepository.java
@@ -1,12 +1,17 @@
 package io.github.jakutenshi.seccomp.organizations;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface OrganizationRepository extends CrudRepository<Organization, Long> {
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
     List<Organization> findByName(String name);
+
+
+    List<Organization> findByLegalAddress(String legalAddress);
 
     @Override
     Optional<Organization> findById(Long aLong);
